3999360cb2149e89ae547d02ffe78b1e707a02a5
TejaswiKuppili <kuppiliteja@gmail.com>
2025-05-19 16:24:33 +0530

    UserController,UserService,UserRepository Updated

---
+++ PatientManagementSystem/PatientManagementSystemAPI/Controllers/Usercontroller.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientManagementSystem.Services.Interfaces;
+namespace PatientManagementSystemAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        /// <summary>
+        /// Retrieves all users with their roles.
+        /// </summary>
+        [HttpGet("with-roles")]
+        public async Task<IActionResult> GetUsersWithRoles()
+        {
+            try
+            {
+                var users = await userService.GetUsersWithRolesAsync();
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                // Optionally log ex here using a logger
+                return StatusCode(500, "An unexpected error occurred while fetching users.");
+            }
+        }
+    }
+
+}
