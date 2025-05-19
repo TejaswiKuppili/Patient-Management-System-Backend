64354be347ae0380cae252c030652552054a115a
TejaswiKuppili <kuppiliteja@gmail.com>
2025-05-19 17:00:06 +0530

    Updated Controller Service and Repo to fetch ApplicationUser details

--- PatientManagementSystem.Data/obj/Debug/net9.0/PatientManagementSystem.Data.AssemblyInfo.cs
+++ PatientManagementSystem.Data/obj/Debug/net9.0/PatientManagementSystem.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b157ceef9ce67fac303ed1a73bd31ed0da7ca47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3999360cb2149e89ae547d02ffe78b1e707a02a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
