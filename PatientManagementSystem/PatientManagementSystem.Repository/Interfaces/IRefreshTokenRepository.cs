0af2b7900d9147163a8c4123926f94bdcfd7f3cb
TejaswiKuppili <kuppiliteja@gmail.com>
2025-05-26 18:50:13 +0530

    Refresh Token Interface

---
+++ PatientManagementSystem/PatientManagementSystem.Repository/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using PatientManagementSystem.Data.Entities;
+namespace PatientManagementSystem.Repository.Interfaces
+{
+    public interface IRefreshTokenRepository
+    {
+        void SaveRefreshToken(RefreshToken refreshToken);
+        RefreshToken GetByToken(string token);
+        void SaveChanges();
+        void RevokeToken(string token);
+
+    }
+}
