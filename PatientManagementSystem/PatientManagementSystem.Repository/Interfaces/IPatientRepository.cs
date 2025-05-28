f3d7fabf81fe3aa1eef2b2d946008f55f08c0adb
TejaswiKuppili <kuppiliteja@gmail.com>
2025-05-28 14:54:04 +0530

    IPatient repo updated

---
+++ PatientManagementSystem/PatientManagementSystem.Repository/Interfaces/IPatientRepository.cs
@@ -0,0 +1,14 @@
+﻿using PatientManagementSystem.Data.Entities;
+
+namespace PatientManagementSystem.Repository.Interfaces
+{
+    public interface IPatientRepository
+    {
+        Task<bool> DeleteAsync(int id);
+        Task UpdateAsync(Patient patient);
+        Task<Patient?> GetByIdAsync(int id);
+        Task<List<Patient>> GetAllAsync();
+        Task AddAsync(Patient patient);
+
+    }
+}
